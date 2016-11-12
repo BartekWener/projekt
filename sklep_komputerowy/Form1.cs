a075bf9cf98f17e431305297d73b925ee0ece2bd
BartkWener <wenerb@o2.pl>
2016-11-12 11:59:39 +0100

    Add project files.

---
+++ sklep_komputerowy/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace sklep_komputerowy
+{
+    public partial class Form1 : Form
+    {
+        private readonly object polaczenie;
+
+        public Form1()
+        {
+            InitializeComponent();
+            połączenieBaza.Text = "Połączono z Bazą";
+        }
+       
+        
+
+        private void połączenieBaza_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+           
+            Close();
+        }
+    }
+}
---
+++ sklep_komputerowy/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace sklep_komputerowy
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            
+            
+            SqlConnection polaczenie = new SqlConnection();
+          polaczenie.ConnectionString = "Data Source=PC\\SQLEXPRESS;Initial Catalog=sklep;Integrated Security=True;"; //ciąg połączeniowy
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+            try
+            {
+                polaczenie.Open();
+           
+               
+
+            }
+            catch (System.Data.SqlClient.SqlException se)
+            {
+                MessageBox.Show("BŁĄD: " + se.StackTrace);
+            }
+            finally
+            {
+                if (polaczenie.State == System.Data.ConnectionState.Open)
+                {
+                    polaczenie.Close();
+                    
+                }
+            }
+        }
+    }
+}
